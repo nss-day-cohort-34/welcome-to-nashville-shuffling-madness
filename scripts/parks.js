bc6b662638325edc61585a9d21619bd664f30083
Allie <allison.tpttn@gmail.com>
2019-07-28 22:14:39 -0500

    made a function to format user input to a valid api property to search for. Then used that property to search the api and return specific data

--- scripts/parks.js
+++ scripts/parks.js
@@ -7,27 +7,71 @@
 const inputContext = document.querySelector("#park__section")
 
 
-// step 1 get the data from API
-const metroFetch = (search) => {
-    fetch("https://data.nashville.gov/resource/74d7-b74t.json")
-        .then(response => response.json())
-        .then((parsedResponse) => {
-    // console.log(parsedResponse[0])
-    for (let i = 0; i < parsedResponse.length; i++) {
-        let parkName = parsedResponse[i].park_name
-        // let parkAddress = parsedResponse[i].map_location.human_address
+// now I have a function (GetParkSearchPropertyFromInput) that takes the input from the park 
+// search by feature and reformats the search value to make it match the valid 
+// search property of the API. With that valid search property, I will search the
+// API for parks who have that property value as true. (have a boat ramp, etc.)
 
-        if (parsedResponse[i].("search") === "Yes") {
-            console.log(parkName)
-        }
+const GetParkSearchPropertyFromInput = (searchInput) => {
+    const lowerCaseSearchInput = searchInput.toLowerCase();
+    switch (lowerCaseSearchInput) {
+        case "basketball court":
+        case "basketball courts":
+            return "basketball_courts";
+
+        case "dog park":
+            return "dog_park";
+
+        case "swimming pool":
+            return "swimming_pool";
 
     }
-})     
-    
 }
 
-metroFetch("boat_launch")
+// This function will be triggered when the search button is clicked and the search
+// input value will be passed to the function
 
+// step 1 get the data from API
+const metroFetch = (search) => {
+    // we need to call the function we made below to get the properly formatted search property
+    // the info i get from the function below will poof if I don't store it in a variable
+    const feature = GetParkSearchPropertyFromInput(search);
+    // user won't put the correct property
+    fetch(`https://data.nashville.gov/resource/74d7-b74t.json?${feature}=Yes`)
+        .then(response => response.json())
+        .then((parsedResponse) => {
+            console.log(parsedResponse)
+
+            // then convert to html and post to DOM
+
+
+            // console.log(parsedResponse[0])
+            // for (let i = 0; i < parsedResponse.length; i++) {
+            //     let parkName = parsedResponse[i].park_name
+            // let parkAddress = parsedResponse[i].map_location.human_address
+
+            // if (parsedResponse[i].("search") === "Yes") {
+            //     console.log(parkName)
+            // }
+            // if (parsedResponse[i][search] === "Yes") {
+            //     console.log(parkName)
+            // }
+            // const i = 0
+            // const firstParsedResponse = parsedResponse[i];
+            // const searchPropertyValue = firstParsedResponse[search];
+            // // OR
+            // firstParsedResponsePropertyValue = parsedResponse[i][search];
+
+
+
+
+            // }
+        })
+
+}
+
+// metroFetch("boat_launch")
+metroFetch("dog PArK");
 
 
 
@@ -53,6 +97,8 @@ metroFetch("boat_launch")
 //     // could save the string in a variable above
 //     inputContext.innerHTML += parkHtml
 // }
+
+
 
 
 
