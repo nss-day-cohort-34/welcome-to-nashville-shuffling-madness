ab40c473f5fa3dc9e2e9a1ca78de8210004c9da1
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-07-29 11:24:06 -0500

    add eventlistener

--- scripts/eventbrite.js
+++ scripts/eventbrite.js
@@ -11,11 +11,47 @@ const eventbriteFetch = (events) => {
     console.log(responses)
     for (let i = 0; i < responses.events.length; i++) {
       const meetUpName = responses.events[i].name.text
-      console.log(meetUpName)
+      const ebHtml = eventbriteHTML(meetUpName)
+      renderToDOMEventbrite(ebHtml)
+
+
       
     }
   
   })
   
 }
-    eventbriteFetch("Music")
+   // eventbriteFetch("Music")
+
+
+    const eventbriteToDOM = document.querySelector("#listContainer__event")
+
+    const eventbriteHTML = (eventbriteItem) => {
+          return `
+          <ol>
+            <li>${eventbriteItem}<button>Save</button></li>
+          </ol>    
+          `
+
+      }
+
+      const renderToDOMEventbrite = (HTMLString) => {
+    
+   
+        eventbriteToDOM.innerHTML += HTMLString
+   }
+
+
+   document.querySelector("#eventbriteButton").addEventListener("click", () => {
+    while (eventbriteToDOM.hasChildNodes()) {
+        eventbriteToDOM.removeChild(eventbriteToDOM.firstChild)
+        // listCounter = 0
+    }
+   const buttonValue = document.querySelector("#eventbriteInput")
+    console.log(buttonValue.value)
+    eventbriteFetch(buttonValue.value) 
+    
+   
+})
+      
+    
