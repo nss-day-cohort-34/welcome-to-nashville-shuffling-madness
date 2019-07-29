e0ba9d897b9f3610ed67c6274f6e514c835cf277
Allie <allison.tpttn@gmail.com>
2019-07-29 10:11:33 -0500

    added an event listener and rendered info to the DOM

--- scripts/parks.js
+++ scripts/parks.js
@@ -22,18 +22,44 @@ const GetParkSearchPropertyFromInput = (searchInput) => {
 
 
 const metroFetch = (search) => {
-    
+
     const feature = GetParkSearchPropertyFromInput(search);
     // user won't put the correct property
     fetch(`https://data.nashville.gov/resource/74d7-b74t.json?${feature}=Yes`)
         .then(response => response.json())
         .then((parsedResponse) => {
             console.log(parsedResponse)
+            for (let i = 0; i < parsedResponse.length; i++) {
+                const parkName = parsedResponse[i].park_name
+                const toHtml = metroHTML(parkName)
+                renderToDOMParks(toHtml)
+                
+            }
         })
 
 }
 
-metroFetch("dog PArK");
+// metroFetch("dog PArK");
 
 
+document.querySelector("#parksButton").addEventListener("click", () => {
+    const buttonValue = document.querySelector("#parksInput")
+    metroFetch(buttonValue.value)
+})
 
+
+const metroHTML = (metroItem) => {
+    return `
+    <ol>
+        <li>${metroItem}<button>Save</button></li>
+    </ol>    
+    `
+}
+
+
+const renderToDOMParks = (HTMLString) => {
+
+    parksContainer.innerHTML += HTMLString
+}
+
+const parksContainer = document.querySelector("#listContainer__parks")
